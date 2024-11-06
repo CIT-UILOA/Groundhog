d604394c6ef2ff9acb24e154d994f660619ab9d1
Kiwifuit <mahkiwi123@gmail.com>
2024-11-06 19:16:21 +0800

    feat: made index mobile-friendly

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import NavItem from "./NavItem";
 export default function Nav() {
   return (
     <nav>
-      <ul class="flex flex-row gap-3 text-4xl font-thin py-3">
+      <ul class="flex flex-row gap-3 text-2xl md:text-4xl font-thin md:py-3 py-2">
         <NavItem href="/" title="Home" />
         <NavItem href="/about" title="About us" />
       </ul>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 export default function Home() {
   return (
-    <main class="m-5">
-      <h1 class="text-9xl w-10 font-extrabold mb-12 text-outline title-layered">
+    <main class="my-1 md:my-5">
+      <h1 class="text-7xl md:text-9xl w-10 font-extrabold mb-12 text-outline title-layered">
         Ground Breaking Technology
       </h1>
-      <p class="whitespace-pre-line text-xl italic w-5/12">
+      <p class="whitespace-pre-line md:text-xl italic md:w-5/12 text-center md:text-left">
         The reasonable man adapts himself to the world: the unreasonable one
         persists in trying to adapt the world to himself. Therefore all progress
         depends on the unreasonable man
       </p>
-      <p class="text-xl">—George Bernard Shaw, Man and Superman (1903)</p>
+      <p class="text-gray-300 md:text-xl text-center md:text-left">
+        —George Bernard Shaw, Man and Superman (1903)
+      </p>
     </main>
   );
 }
