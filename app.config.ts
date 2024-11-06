11fe76a236c232246e174e174824f2d012216d6a
Kiwifuit <mahkiwi123@gmail.com>
2024-11-07 05:30:06 +0800

    some more tweaking
    please make it work 😭

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   ssr: true,
-  server: { preset: "static", baseURL: process.env.BASE_PATH },
+  server: { preset: "static", baseURL: process.env.SERVER_BASE_URL || "/" },
+  vite: {
+    base: process.env.SERVER_BASE_URL || "/",
+  },
 });
